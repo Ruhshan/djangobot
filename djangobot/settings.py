c516fa35871fd0c0d242295d43e455fa8ed0ee94
Ruhshan <ruhshan.ahmed@gmail.com>
2017-10-29 13:06:44 +0600

    static updated

--- djangobot/settings.py
+++ djangobot/settings.py
@@ -17,7 +17,7 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/static_media/"
 
 # Quick-start development settings - unsuitable for production
