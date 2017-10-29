399930553218145ba3560b1d52f1821fb33f1b71
Ruhshan <ruhshan.ahmed@gmail.com>
2017-10-29 13:42:57 +0600

    dfda

--- djangobot/settings.py
+++ djangobot/settings.py
@@ -143,6 +143,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_ROOT, "static"),
 ]
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
