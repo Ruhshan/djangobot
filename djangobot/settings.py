e30990cdad59e8587fcb252efb2e8d2a810b4c87
Ruhshan <ruhshan.ahmed@gmail.com>
2017-10-29 13:12:10 +0600

    added project root in settings

--- djangobot/settings.py
+++ djangobot/settings.py
@@ -16,6 +16,7 @@ from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/static_media/"
